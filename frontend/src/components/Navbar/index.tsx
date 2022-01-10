5d6a7685d6c0a4bc94e15c34685f6fd6a8b7ba8b
Felipe Martins <fe.mmo515@gmail.com>
2022-01-10 19:32:46 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,21 @@
+import { ReactComponent as GithubIcon } from 'assets/img/Github.svg';
+import './styles.css'
+
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>FnMovie</h1>
+          <a href="https://github.com/devsuperior">
+            <div className="fnmovie-contact-container">
+                 <GithubIcon />
+              <p className="fnmovie-contact-link">/Valossa515</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  )
+}
+export default Navbar;
