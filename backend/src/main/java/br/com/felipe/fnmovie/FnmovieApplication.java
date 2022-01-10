91a1b7970ce8ceffb978ef852aa82071a4702883
Felipe Martins <fe.mmo515@gmail.com>
2022-01-10 17:08:08 -0300

    Project Created

---
+++ backend/src/main/java/br/com/felipe/fnmovie/FnmovieApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipe.fnmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FnmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/felipe/fnmovie/FnmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipe.fnmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
