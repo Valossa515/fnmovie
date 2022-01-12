21df005911572c9895905b660ae876ab008e9737
Felipe Martins <fe.mmo515@gmail.com>
2022-01-12 11:07:41 -0300

    Find movies

---
+++ backend/src/main/java/br/com/felipe/fnmovie/controllers/MovieController.java
@@ -0,0 +1,31 @@
+package br.com.felipe.fnmovie.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.felipe.fnmovie.DTO.MovieDTO;
+import br.com.felipe.fnmovie.services.MovieService;
+
+@RestController
+@RequestMapping(value = "/movies")
+public class MovieController {
+	
+	@Autowired
+	private MovieService movieService;
+	
+	@GetMapping
+	public Page<MovieDTO> findAll(Pageable pageable){
+		return movieService.finAll(pageable);
+	}
+	
+	@GetMapping(value="/{id}")
+	public MovieDTO findById(@PathVariable Long id){
+		return movieService.finById(id);
+	}
+	
+}
---
+++ backend/src/main/java/br/com/felipe/fnmovie/repositories/MovieRespository.java
@@ -0,0 +1,9 @@
+package br.com.felipe.fnmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.felipe.fnmovie.entities.Movie;
+
+public interface MovieRespository extends JpaRepository<Movie, Long>{
+	
+}
