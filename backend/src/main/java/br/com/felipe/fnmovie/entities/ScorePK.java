e1fdccd5b581e9d1fce5d9b4288aa48aa2d4700e
Felipe Martins <fe.mmo515@gmail.com>
2022-01-12 09:04:41 -0300

    Domain model, database seed

---
+++ backend/src/main/java/br/com/felipe/fnmovie/entities/ScorePK.java
@@ -0,0 +1,42 @@
+package br.com.felipe.fnmovie.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class ScorePK implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	@JoinColumn(name = "movie_id")
+	private Movie movie;
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User usre;
+	
+	public ScorePK() {
+		
+	}
+	
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	public User getUsre() {
+		return usre;
+	}
+
+	public void setUsre(User usre) {
+		this.usre = usre;
+	}
+	
+	
+	
+}
