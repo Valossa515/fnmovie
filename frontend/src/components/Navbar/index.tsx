c514bc4f9d95b1290634158f79d93feb0d08870a
Felipe Martins <fe.mmo515@gmail.com>
2022-01-12 08:19:53 -0300

    Security Config

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
       <nav className="container">
         <div className="dsmovie-nav-content">
           <h1>FnMovie</h1>
-          <a href="https://github.com/devsuperior">
+          <a href="https://github.com/Valossa515/fnmovie">
             <div className="fnmovie-contact-container">
                  <GithubIcon />
               <p className="fnmovie-contact-link">/Valossa515</p>
